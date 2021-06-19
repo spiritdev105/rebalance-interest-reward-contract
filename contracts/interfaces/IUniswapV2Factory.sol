71b2b1fd81ee2c221af3fdc6c8fd36578b4c2af0
spiritdev105 <spiritdev105@gmail.com>
2021-06-19 12:50:21 +0800

    remove non-essential contracts

--- contracts/interfaces/IUniswapV2Factory.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-
-pragma solidity ^0.8.0;
-
-interface IUniswapV2Factory {
-
-  function getPair(address _fromToken, address _toToken) external view returns(address);
-  function createPair(address tokenA, address tokenB) external returns (address pair);
-}
